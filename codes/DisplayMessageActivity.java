5bbc04af75b260e2355697b162e85de171d94082
RRShanbhag <ranjit.shanbhag@gmail.com>
2018-07-22 13:27:20 -0600

    Save Full Size Image in External Storage.

--- codes/DisplayMessageActivity.java
+++ codes/DisplayMessageActivity.java
@@ -1,8 +1,13 @@
 package com.shanbhag.ranjit.myfirstapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +15,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DisplayMessageActivity extends AppCompatActivity {
 
     ImageView imageView;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +43,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
+                dispatchTakePictureIntent();
             }
 
         });
@@ -43,11 +52,62 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            //super.onActivityResult(requestCode, resultCode, data);
-            Bitmap bitmap;
-            bitmap = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(bitmap);
+        super.onActivityResult(requestCode, resultCode, data);
+        /* //Testing..
+        if(RESULT_OK == resultCode){
+            galleryAddPic();
+            imageView = findViewById(R.id.imageView);
+
+            //Decode it for real
+            BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+            bmpFactoryOptions.inJustDecodeBounds = false;
+
+            //imageFilePath image path which you pass with intent
+            Bitmap bmp = BitmapFactory.decodeFile(mCurrentPhotoPath, bmpFactoryOptions);
+
+            //Display it
+            imageView.setImageBitmap(bmp);
         }
+        */
+        Bitmap takenImage = BitmapFactory.decodeFile(mCurrentPhotoPath);
+        // RESIZE BITMAP, see section below
+        // Load the taken image into a preview
+        ImageView imgPreview = (ImageView) findViewById(R.id.imageView);
+        imgPreview.setImageBitmap(takenImage);
+    }
+    private  void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try{
+                photoFile = getPictureFile();
+            }catch (IOException ex){
+
+            }
+            if( photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.shanbhag.ranjit.myfirstapp.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    protected File getPictureFile() throws IOException {
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String pictureFile = "JPEG_"+timestamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(pictureFile, ".jpg", storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic(){
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
